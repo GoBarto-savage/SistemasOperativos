5e2dd41631fa74475aa64d790f219e924143a734
T0w3r <torres.alejo2001@gmail.com>
2021-08-10 21:47:21 -0300

    Fixed quitar puntas

--- interbloqueo/Program.cs
+++ interbloqueo/Program.cs
@@ -8,13 +8,72 @@ namespace interbloqueo
 {
     class Program
     {
+        public static void Desmarcar_todo(List<Vertice> vertices)
+        {
+            foreach (Vertice v in vertices)
+            {
+                v.Marcado = false;
+            }
+        }
+        public static void Camino(Vertice oVertice, string sangria = "")
+        {
+            if (oVertice != null && oVertice.Marcado == false)
+            {
+                oVertice.Marcado = true;
+                Console.WriteLine(sangria + oVertice.Valor);
+                File.AppendAllText(System.IO.Path.GetFullPath("Resultados.txt"), sangria + oVertice.Valor);
+
+                foreach (var oV in oVertice.Aristas)
+                {
+                    Camino(oV, sangria + "\t");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine(sangria + oVertice.Valor);
+                Console.WriteLine(sangria + "\t" + "¡INTERBLOQUEO DETECTADO! ");
+                File.AppendAllText(System.IO.Path.GetFullPath("Resultados.txt"), sangria + oVertice.Valor + Environment.NewLine);
+                File.AppendAllText(System.IO.Path.GetFullPath("Resultados.txt"), sangria + "¡INTERBLOQUEO DETECTADO! ");
+            }
+        }
+        public static List<Vertice>  QuitarPuntas(List<Vertice> todos_vertices) {
+            foreach (Vertice vertice in todos_vertices.ToArray())
+            {
+                bool es_punta = true;
+                Vertice punta = vertice;
+                foreach (Vertice vertice2 in todos_vertices)
+                {
+                    if (vertice != vertice2)
+                    {
+                        foreach (var arista in vertice2.Aristas)
+                        {
+                            if (vertice.Valor == arista.Valor)
+                            {
+                                es_punta = false;
+
+
+                            }
+
+                        }
+
+                    }
+                }
+                if (es_punta)
+                {
+                    Console.WriteLine(vertice.Valor + " Es PUNTA");
+                    todos_vertices.Remove(vertice); //al remover la punta da la posibilidad de nuevas puntas, que son retiradas por el mismo algortimo
+
+                }
+
+            }
+            return todos_vertices;
+
+        }
         static void Main(string[] args)
         {
             //Usando como modelo el grafo dirigido (a) dibujado en la diapositiva 19 del pdf Interbloqueos en la plataforma
             //Instancio todos los vertices
-            List<Vertice> todos_verticesArray = new List<Vertice>();
-
-
             Vertice oVertice_R = new Vertice("R");
             Vertice oVertice_A = new Vertice("A");
             Vertice oVertice_C = new Vertice("C");
@@ -30,24 +89,7 @@ namespace interbloqueo
             Vertice oVertice_B = new Vertice("B");
 
             //Almaceno todos los grados en una lista
-            Vertice[] todos_vertices = new Vertice[13] { oVertice_R, oVertice_A, oVertice_C, oVertice_W, oVertice_F, oVertice_S, oVertice_D, oVertice_U, oVertice_G, oVertice_V, oVertice_E, oVertice_T, oVertice_B };
-
-            todos_verticesArray.Add(oVertice_R);
-            todos_verticesArray.Add(oVertice_A);
-            todos_verticesArray.Add(oVertice_C);
-            todos_verticesArray.Add(oVertice_W);
-            todos_verticesArray.Add(oVertice_F);
-            todos_verticesArray.Add(oVertice_S);
-            todos_verticesArray.Add(oVertice_D);
-            todos_verticesArray.Add(oVertice_U);
-            todos_verticesArray.Add(oVertice_G);
-            todos_verticesArray.Add(oVertice_V);
-            todos_verticesArray.Add(oVertice_E);
-            todos_verticesArray.Add(oVertice_T);
-            todos_verticesArray.Add(oVertice_B);
-
-            
-
+            List<Vertice> todos_vertices = new List<Vertice> { oVertice_R, oVertice_A, oVertice_C, oVertice_W, oVertice_F, oVertice_S, oVertice_D, oVertice_U, oVertice_G, oVertice_V, oVertice_E, oVertice_T, oVertice_B };
             //Arcos correspondientes
             // "R" QUIERE A "A"
             oVertice_R.Aristas.Add(oVertice_A);
@@ -76,36 +118,7 @@ namespace interbloqueo
             // "U" QUIERE A "D"
             oVertice_U.Aristas.Add(oVertice_D);
 
-            foreach (Vertice vertice in todos_vertices)
-            {
-                bool es_punta = true;
-                Vertice punta = vertice;
-                foreach (Vertice vertice2 in todos_vertices)
-                {
-                    if (vertice != vertice2)
-                    {
-                        foreach (var arista in vertice2.Aristas)
-                        {
-                            if (vertice.Valor == arista.Valor)
-                            {
-                                //si es punta no la incorporo al array list
-                                todos_verticesArray.Remove(vertice);
-                                es_punta = false;
-                            }
-                            else
-                            {
-                              
-                            }
-                        }
-
-                    }
-                }
-                if (es_punta)
-                {
-                    Console.WriteLine(vertice.Valor + " Es PUNTA");
-                }
-
-            }
+           
 
 
             using (StreamWriter writetext = new StreamWriter(System.IO.Path.GetFullPath("Resultados.txt")))
@@ -139,66 +152,23 @@ namespace interbloqueo
             Camino(oVertice_B);
             Desmarcar_todo(todos_vertices);
              */
-
-
+            todos_vertices = QuitarPuntas(todos_vertices);
             //Si queremos recorrer ABSOLUTAMENTE TODOS LOS NODOS podemos hacerlo asi:
             File.AppendAllText(System.IO.Path.GetFullPath("Resultados.txt"), "---RECORRIDOS---" + Environment.NewLine);
 
-            for (int i = 0; i < todos_verticesArray.Count; i++)
-            {
-                Console.WriteLine(todos_verticesArray[i].Valor);
-                
-            }
-
-            for (int i=0;i< todos_verticesArray.Count; i++)
-            {
-                Console.WriteLine("\b" + "Recorrido del nodo " + todos_verticesArray[i].Valor);
-                File.AppendAllText(System.IO.Path.GetFullPath("Resultados.txt"), Environment.NewLine + "Recorrido del nodo " + todos_verticesArray[i].Valor + Environment.NewLine);
-                Camino(todos_verticesArray[i]);
-                Desmarcar_todo(todos_vertices);
-            }
             
-            /*
              foreach (Vertice v in todos_vertices)
             {
                 Console.WriteLine("\b" + "Recorrido del nodo " + v.Valor);
                 File.AppendAllText(System.IO.Path.GetFullPath("Resultados.txt"), Environment.NewLine + "Recorrido del nodo " + v.Valor + Environment.NewLine);
                 Camino(v);
                 Desmarcar_todo(todos_vertices);
-            }*/
+            }
             
         }
 
 
 
-        public static void Desmarcar_todo(Vertice[] vertices)
-        {
-            foreach (Vertice v in vertices)
-            {
-                v.Marcado = false;
-            }
-        }
-        public static void Camino(Vertice oVertice, string sangria = "")
-        {
-            if (oVertice != null && oVertice.Marcado == false)
-            {
-                oVertice.Marcado = true;
-                Console.WriteLine(sangria + oVertice.Valor);
-                File.AppendAllText(System.IO.Path.GetFullPath("Resultados.txt"), sangria + oVertice.Valor);
-
-                foreach (var oV in oVertice.Aristas)
-                {
-                    Camino(oV, sangria + "\t");
-                }
-
-            }
-            else
-            {
-                Console.WriteLine(sangria + oVertice.Valor);
-                Console.WriteLine(sangria + "\t" + "¡INTERBLOQUEO DETECTADO! ");
-                File.AppendAllText(System.IO.Path.GetFullPath("Resultados.txt"), sangria + oVertice.Valor + Environment.NewLine);
-                File.AppendAllText(System.IO.Path.GetFullPath("Resultados.txt"), sangria + "¡INTERBLOQUEO DETECTADO! ");
-            }
-        }
+       
     }
 }
